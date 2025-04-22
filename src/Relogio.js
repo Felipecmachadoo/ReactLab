37ed514933fa0ac1be16570b3045fb0d2dc81e08
Felipe Machado <felpsmachadoo@gmail.com>
2025-04-22 11:03:17 -0300

    Add files via upload

---
+++ src/Relogio.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Relogio() {
+    return (
+        <div>
+            <h2>Aqui é o relógio</h2>
+        </div>
+    )
+}
+
+
+
+export default Relogio
