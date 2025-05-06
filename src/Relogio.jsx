aa9c4e25b418c2c574621f29cded16e7b0ffa5cb
Felipe Machado <felpsmachadoo@gmail.com>
2025-05-05 21:20:08 -0300

    Criando um relógio
    Criando um relógio com a hora atual que atualiza a cada segundo para a atividade 1

--- src/Relogio.jsx
+++ src/Relogio.jsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 function Relogio() {
-    return (
-        <div>
-            <h2>Aqui é o relógio</h2>
-        </div>
-    )
+  const [hora, setHora] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setHora(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(intervalo);
+  }, []);
+
+  return (
+    <div
+      style={{ fontSize: "3rem", fontFamily: "monospace", textAlign: "center" }}
+    >
+      {hora}
+    </div>
+  );
 }
 
-
-
-export default Relogio
+export default Relogio;
